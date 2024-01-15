3b9f66f88b3a8e98ec112df25b73eea789f6d155
Evan Fleming <evan.gordon.fleming@gmail.com>
2024-01-15 11:09:44 -0800

    Add all completed exercises

---
+++ README.md
@@ -0,0 +1,3 @@
+# rustlings
+
+All excersises from the [rustlings](https://github.com/rust-lang/rustlings) project
---
+++ exercises/clippy/target/debug/deps/clippy3-856a3cbbed71bfb0.d
@@ -0,0 +1,9 @@
+/Users/eflem/rustlings/exercises/clippy/target/debug/deps/libclippy3-856a3cbbed71bfb0.rmeta: clippy3.rs Cargo.toml
+
+/Users/eflem/rustlings/exercises/clippy/target/debug/deps/clippy3-856a3cbbed71bfb0.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
